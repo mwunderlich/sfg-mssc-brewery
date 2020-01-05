bdc972fcbbca58273c58ac9753e6323ca3aaae9f
Martin Wunderlich <martin.wunderlich@censhare.com>
2020-01-05 13:43:08 +0100

    Initial commit (after spring initializer setup)

---
+++ src/main/java/guru/springframework/sfgmsscbrewery/SfgMsscBreweryApplication.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgmsscbrewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SfgMsscBreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SfgMsscBreweryApplication.class, args);
+	}
+
+}
---
+++ src/test/java/guru/springframework/sfgmsscbrewery/SfgMsscBreweryApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.sfgmsscbrewery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SfgMsscBreweryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
